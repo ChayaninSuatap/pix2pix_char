ba8888c7771c426f8dd4e736085ccaba037be9bd
Arm <chayanin.suatap@gmail.com>
2019-10-01 15:18:12 +0700

    add augment

---
+++ augment_util.py
@@ -0,0 +1,33 @@
+from PIL import Image, ImageOps
+import random
+
+def flip_left_right(img):
+    return img.transpose(Image.FLIP_LEFT_RIGHT)
+    
+
+def flip_up_down(img):
+    return img.transpose(Image.FLIP_TOP_BOTTOM)
+
+def rotate(img, angle):
+    img = ImageOps.invert(img)
+    img = img.rotate(angle=angle, expand=1)
+    img = ImageOps.invert(img)
+    return img
+
+def augment(x_img, y_img, img_x_resize, img_y_resize):
+    if random.randint(0, 1) == 0:
+        x_img = flip_left_right(x_img)
+        y_img = flip_left_right(y_img)
+    if random.randint(0, 1) == 0:
+        x_img = flip_up_down(x_img)
+        y_img = flip_up_down(y_img)
+    angle = random.randint(0, 360)
+    x_img = rotate(x_img, angle).resize(img_x_resize)
+    y_img = rotate(y_img, angle).resize(img_y_resize)
+
+    return x_img, y_img
+
+if __name__ == '__main__':
+    img = Image.open('x_path/4_1.jpg')
+    img = rotate(img)
+    img.show()
--- dataset_util.py
+++ dataset_util.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 import random
-
+from augment_util import augment
 def read_img(path, size):
     img = Image.open(path)
     img = img.resize(size)
@@ -31,7 +31,6 @@ def make_dataset_generator(batch_size, img_x_resize=(40,40), img_y_resize=(40,40
     for fn in os.listdir(x_path):
         img = Image.open(x_path + fn)
         img = img.resize(img_x_resize)
-        img = np.asarray(img) / 127.5 - 1
         label = int(fn[2:-4]) - 1
         x_imgs.append( (img, label))
     
@@ -43,7 +42,6 @@ def make_dataset_generator(batch_size, img_x_resize=(40,40), img_y_resize=(40,40
             label = int(foldername) - 1
             img = Image.open(y_path+foldername+'/'+fn)
             img = img.resize(img_y_resize)
-            img = np.asarray(img) / 127.5 - 1
             y_imgs.append( (img, label))
 
     y_img_dict = {}
@@ -63,6 +61,13 @@ def make_dataset_generator(batch_size, img_x_resize=(40,40), img_y_resize=(40,40
         label = x_imgs[x_img_i][1] #0=img, 1=label
         y_img_i = random.randint(0, len(y_img_dict[label]) - 1)
         y_img = y_img_dict[label][y_img_i]
+
+        #augment
+        x_img , y_img = augment(x_img, y_img, img_x_resize, img_y_resize)
+        #asarray to normalize
+        x_img = np.asarray(x_img) / 127.5 - 1
+        y_img = np.asarray(y_img) / 127.5 - 1
+        
         img_x_chrunk.append( x_img.reshape(img_x_resize[0], img_x_resize[1], 1))
         img_y_chrunk.append( y_img.reshape(img_y_resize[0], img_y_resize[1], 1))
         label_chrunk.append( label)
